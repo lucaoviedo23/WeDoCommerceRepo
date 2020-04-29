a60781a5b8f97bc4824fdcc57fd0b3d9596b62f3
Luca Federico Oviedo <lucamop@gmail.com>
2020-04-29 15:54:32 -0300

    AddProduct
    Falta uploadimage

--- WeDoAngularWeb/src/app/app-routing.module.ts
+++ WeDoAngularWeb/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddproductsComponent } from './components/products/addproducts/addproducts.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: '', component: AddproductsComponent }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

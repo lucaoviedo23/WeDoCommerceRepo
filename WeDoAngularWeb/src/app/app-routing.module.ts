f710a6d9b9900d7389da14277a2da1df65dbc039
Luca Federico Oviedo <lucamop@gmail.com>
2020-04-30 19:43:02 -0300

    .
    .

--- WeDoAngularWeb/src/app/app-routing.module.ts
+++ WeDoAngularWeb/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddproductsComponent } from './components/products/addproducts/addproducts.component';
 import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { EditproductsComponent } from './components/products/editproducts/editproducts.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot([
-    { path: '', component: AddproductsComponent }
+    { path: '', component: EditproductsComponent }
   ])],
   exports: [RouterModule]
 })
--- WeDoAngularWeb/src/app/components/products/editproducts/editproducts.component.ts
+++ WeDoAngularWeb/src/app/components/products/editproducts/editproducts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-editproducts',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editproducts.component.css']
 })
 export class EditproductsComponent implements OnInit {
-
+  @ViewChild("inputMarca",{static: false}) inputMarca: ElementRef;
+  @ViewChild("inputNombre",{static: false}) inputNombre: ElementRef;
+  @ViewChild("inputDesc",{static: false}) inputDesc: ElementRef;
+  @ViewChild("inputCod",{static: false}) inputCod: ElementRef;
+  nompreProducto:any;
   constructor() { }
 
   ngOnInit() {
   }
 
+  cleanInputs():void{
+    this.inputMarca.nativeElement.value = null;
+    this.inputNombre.nativeElement.value = null;
+    this.inputDesc.nativeElement.value = null;
+    this.inputCod.nativeElement.value = null;
+  }
 }

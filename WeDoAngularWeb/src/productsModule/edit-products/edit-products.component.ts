092017688aeb744e3d95b6b261307df222b3e05b
Luca Federico Oviedo <lucamop@gmail.com>
2020-05-06 10:16:23 -0300

    editProducts agregado

--- WeDoAngularWeb/src/productsModule/edit-products/edit-products.component.ts
+++ WeDoAngularWeb/src/productsModule/edit-products/edit-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-edit-products',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-products.component.css']
 })
 export class EditProductsComponent implements OnInit {
-
+  @ViewChild("inputMarca",{static: false}) inputMarca: ElementRef;
+  @ViewChild("inputNombre",{static: false}) inputNombre: ElementRef;
+  @ViewChild("inputDesc",{static: false}) inputDesc: ElementRef;
+  @ViewChild("inputCod",{static: false}) inputCod: ElementRef;
+  nompreProducto:any;
   constructor() { }
 
   ngOnInit() {
   }
 
+  cleanInputs():void{
+    this.inputMarca.nativeElement.value = null;
+    this.inputNombre.nativeElement.value = null;
+    this.inputDesc.nativeElement.value = null;
+    this.inputCod.nativeElement.value = null;
+  }
 }
--- WeDoAngularWeb/src/productsModule/products-routing.module.ts
+++ WeDoAngularWeb/src/productsModule/products-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddProductsComponent } from './add-products/add-products.component';
+import { EditProductsComponent } from './edit-products/edit-products.component';
 
 
 const routes: Routes = [
   { path: 'add', component: AddProductsComponent },
+  { path: 'edit', component: EditProductsComponent },
 
 ];
 
